deb5c0b5d3ee89da265cb9c5fded9d62b17cbfbe
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-08-18 10:08:40 +0300

    Started tests for FA2 NFT

--- test/fa2_nft/fa2_1_transfer.test.js
+++ test/fa2_nft/fa2_1_transfer.test.js
@@ -7,7 +7,6 @@ contract("FA2 Non Fungible Token Contract - Transfers", () => {
   let fa2_address;
   let fa2_instance;
   let signerFactory;
-  let tokenId = 0;
 
   before(async () => {
     const config = await setup();
@@ -24,32 +23,86 @@ contract("FA2 Non Fungible Token Contract - Transfers", () => {
   });
 
   it("should mint 1 token for Alice", async () => {
-    try {
-      const decimals = 0;
-      const extras = new MichelsonMap();
-      const name = "test";
-      const symbol = "TST";
-      const ownersAddress = [alice.pkh];
-      const rangeFrom = 1;
-      const rangeTo = 1;
-      tokenId++;
+    const decimals = 0;
+    const extras = new MichelsonMap();
+    const name = "test";
+    const symbol = "TST";
+    const tokenId = Date.now();
+    const owners = [alice.pkh];
+    const rangeFrom = 0;
+    const rangeTo = 1;
 
+    try {
       const op = await fa2_instance.methods
-        .mint_tokens([
+        .mint_tokens(
           decimals,
           extras,
           name,
           symbol,
           tokenId,
-          ownersAddress,
+          owners,
           rangeFrom,
           rangeTo
-        ])
+        )
         .send();
-      console.log(op.opHash);
       await op.confirmation();
     } catch (error) {
       console.log(error);
     }
+
+    storage = await fa2_instance.storage();
+    const aliceAddress = await storage.assets.ledger.get("0");
+
+    assert.equal(aliceAddress, alice.pkh);
+
+    const mintedToken = await storage.assets.metadata.metadata.get({
+      0: rangeFrom,
+      1: rangeTo
+    });
+
+    assert.equal(tokenId, mintedToken.token_id.toNumber());
+    assert.equal(name, mintedToken.name);
+  });
+
+  it("should mint 3 tokens for Bob", async () => {
+    const decimals = 0;
+    const extras = new MichelsonMap();
+    const name = "bobCoin";
+    const symbol = "BBC";
+    const tokenId = Date.now();
+    const owners = [bob.pkh, bob.pkh, bob.pkh];
+    const rangeFrom = storage.assets.metadata.last_used_id.toNumber();
+    const rangeTo = storage.assets.metadata.last_used_id.toNumber() + 3;
+
+    try {
+      const op = await fa2_instance.methods
+        .mint_tokens(
+          decimals,
+          extras,
+          name,
+          symbol,
+          tokenId,
+          owners,
+          rangeFrom,
+          rangeTo
+        )
+        .send();
+      await op.confirmation();
+    } catch (error) {
+      console.log(error);
+    }
+
+    storage = await fa2_instance.storage();
+    const bobAddress = await storage.assets.ledger.get("1");
+
+    assert.equal(bobAddress, bob.pkh);
+
+    const mintedToken = await storage.assets.metadata.metadata.get({
+      0: rangeFrom,
+      1: rangeTo
+    });
+
+    assert.equal(tokenId, mintedToken.token_id.toNumber());
+    assert.equal(name, mintedToken.name);
   });
 });
